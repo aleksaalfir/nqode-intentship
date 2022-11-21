46ecd37abebe1e52e8871fbb97e91e4900abaafa
Aleksa Alfirovic <aleksaalfirovic@Aleksas-MacBook-Pro.local>
2022-11-21 13:37:18 +0100

    Rebase to develop

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,7 @@ import ProfilePage from 'pages/ProfilePage';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import UserBookPage from 'pages/User/UserBookPage';
 import ManageUsersPage from 'pages/Admin/ManageUsersPage';
-<<<<<<< HEAD
-import EditBookPage from 'pages/Admin/EditBookPage';
-import RentedBooksPage from 'pages/Admin/RentedBooksPage';
-import RentedBookPage from 'pages/Admin/RentedBookPage';
-=======
 import routeService from 'services/routeService';
->>>>>>> 13a6a2d (NavBar links and routes implemented)
 
 const App = () => {
   const { getAllowedRoutes, getRedirect } = routeService;
@@ -20,22 +14,10 @@ const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-<<<<<<< HEAD
-        <Route path="/login" element={<Login />} />
-        <Route path="/books" element={<AdminBooksPage />} />
-        <Route path="/profile" element={<ProfilePage />} />
-        <Route path="/book/:id" element={<UserBookPage />} />
-        <Route path="/create-book" element={<CreateBookPage />} />
-        <Route path="/manage-users" element={<ManageUsersPage />} />
-        <Route path="/book/edit/:id" element={<EditBookPage />} />
-        <Route path="/rents" element={<RentedBooksPage />} />
-        <Route path="/rent/:id" element={<RentedBookPage />} />
-=======
         {getAllowedRoutes().map((route, index) => {
           return <Route path={route.path} element={route.element} key={index} />;
         })}
         {getRedirect()}
->>>>>>> 13a6a2d (NavBar links and routes implemented)
       </Routes>
     </BrowserRouter>
   );
