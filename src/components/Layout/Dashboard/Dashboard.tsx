24c394762431ee596c8dd094bb42b3e462c2eaba
Aleksa Alfirovic <aleksaalfirovic@Aleksas-MacBook-Pro.local>
2022-11-23 13:02:34 +0100

    Admin dashboard section can now be closed or opened

--- src/components/Layout/Dashboard/Dashboard.tsx
+++ src/components/Layout/Dashboard/Dashboard.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Outlet, useLocation, useNavigate } from 'react-router-dom';
 import classes from './Dashboard.module.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -8,7 +8,9 @@ import {
   faUser,
   faUsers,
   faChartSimple,
-  faRightFromBracket
+  faRightFromBracket,
+  faArrowRight,
+  faArrowLeft
 } from '@fortawesome/free-solid-svg-icons';
 import DashboardLink from './DashboardLink';
 import { logout } from 'services/authService';
@@ -17,6 +19,7 @@ import path from 'path';
 const Dashboard = () => {
   const { pathname } = useLocation();
   const navigate = useNavigate();
+  const [toggleSections, setToggleSections] = useState(true);
   useEffect(() => {
     if (pathname === '/dashboard' || pathname === '/dashboard/') {
       navigate('/dashboard/books');
@@ -25,21 +28,38 @@ const Dashboard = () => {
 
   return (
     <div className={classes['c-dashboard']}>
-      <div className={classes['c-dashboard__section']}>
-        <div className={classes['c-dashboard__section-main']}>
-          <DashboardLink icon={faBook} text={'Books'} url={'/dashboard/books'} />
-          <DashboardLink icon={faPlus} text={'Add book'} url={'/dashboard/create-book'} />
-          <DashboardLink icon={faChartSimple} text={'Rents'} url={'/dashboard/rents'} />
-          <DashboardLink icon={faUsers} text={'Users'} url={'/dashboard/manage-users'} />
+      {toggleSections ? (
+        <div className={classes['c-dashboard__section']}>
+          <div className={classes['c-dashboard__section-main']}>
+            <div className={classes['c-dashboard__section-arrow']}>
+              <FontAwesomeIcon
+                icon={faArrowLeft}
+                onClick={() => setToggleSections(!toggleSections)}
+              />
+            </div>
+            <DashboardLink icon={faBook} text={'Books'} url={'/dashboard/books'} />
+            <DashboardLink icon={faPlus} text={'Add book'} url={'/dashboard/create-book'} />
+            <DashboardLink icon={faChartSimple} text={'Rents'} url={'/dashboard/rents'} />
+            <DashboardLink icon={faUsers} text={'Users'} url={'/dashboard/manage-users'} />
+          </div>
+          <div className={classes['c-dashboard__section-secondary']}>
+            <DashboardLink icon={faUser} text={'Profile'} url={'/dashboard/profile'} />
+            <li className={classes['c-dashboard__nav-link']} onClick={() => logout()}>
+              <FontAwesomeIcon icon={faRightFromBracket} className={classes['c-dashboard__icon']} />
+              Log out
+            </li>
+          </div>
         </div>
-        <div className={classes['c-dashboard__section-secondary']}>
-          <DashboardLink icon={faUser} text={'Profile'} url={'/dashboard/profile'} />
-          <li className={classes['c-dashboard__nav-link']} onClick={() => logout()}>
-            <FontAwesomeIcon icon={faRightFromBracket} className={classes['c-dashboard__icon']} />
-            Log out
-          </li>
+      ) : (
+        <div className={classes['c-dashboard__section-closed']}>
+          <div className={classes['c-dashboard__section-arrow']}>
+            <FontAwesomeIcon
+              icon={faArrowRight}
+              onClick={() => setToggleSections(!toggleSections)}
+            />
+          </div>
         </div>
-      </div>
+      )}
       <div className={classes['c-dashboard__content']}>
         <Outlet />
       </div>
