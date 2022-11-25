9e7befd4e7e95fc5a4fbb22816dc83b3a6a1faef
Aleksa Alfirovic <aleksaalfirovic@Aleksas-MacBook-Pro.local>
2022-11-25 16:08:10 +0100

    Eslint ignored

--- src/pages/ProfilePage/ProfilePage.tsx
+++ src/pages/ProfilePage/ProfilePage.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useState, useEffect } from 'react';
 import RentedBookCopyOverview from 'model/RentedBookCopyOverview';
 import { getIdFromJwt } from 'services/authService';
